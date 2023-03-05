85601cbfc6aea9d0aebc5f5e4921ff20f7de24bc
RijiyZ <ivandikarev3000@gmail.com>
2023-03-05 21:02:17 +0300

    функц

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        String text = scanner.nextLine();
+
+        String[] words = text.split("\\s+");
+        int wordCount = words.length;
+        System.out.println("Всего в тексте " + wordCount);
+
+        Map<String, Integer> frequencyTable = new HashMap<>();
+        for (String word : words) {
+            if (frequencyTable.containsKey(word)) {
+                frequencyTable.put(word, frequencyTable.get(word) + 1);
+            } else {
+                frequencyTable.put(word, 1);
+            }
+        }
+
+        System.out.println("Top 10:");
+
+        List<Map.Entry<String, Integer>> frequencyList = new ArrayList<>(frequencyTable.entrySet());
+        frequencyList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(frequencyList.get(i).getKey() + ": " + frequencyList.get(i).getValue());
+
+        }
+    }
+}
